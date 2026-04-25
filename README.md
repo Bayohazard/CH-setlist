87be42b19de3a40b04b0cdf9dca58fb6c200cc0e
Bayohazard <42082719+Bayohazard@users.noreply.github.com>
2026-04-25 10:30:42 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My Clone Hero Setlist
+
+Powered by [Mevami](https://github.com/s3vro-h1/mevami)
---
+++ config.py
@@ -0,0 +1,3 @@
+#Check the readme to see how the path should look like on each OS
+SONGS_PATH ="/Users/Bryan/Clone Hero/Songs" 
+OS = "MAC"
